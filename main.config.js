4befa7dbac2dd1223e511edebad125237d3ec435
caiozhang <caiozhang@tencent.com>
2021-08-26 20:02:17 +0800

    feat: 支持接口分类使用secret

--- main.config.js
+++ main.config.js
@@ -1,5 +1,8 @@
 module.exports = {
     corpid:'ww827c549fa062654e',
     secret:'***REMOVED***',
-    robot:'YOUR_ROBOT_WEBHOOK'
+    robot:'YOUR_ROBOT_WEBHOOK',
+    address: {
+        secret: '***REMOVED***'
+    }
 }
