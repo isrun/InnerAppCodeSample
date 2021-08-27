24ee88f8d2840b8e5b0103919351e58d6282905e
caiozhang <caiozhang@tencent.com>
2021-08-27 16:43:43 +0800

    chroe: 移除需要ignore的文件

--- main.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    corpid:'ww827c549fa062654e',
-    secret:{
-        common: '***REMOVED***',
-        contact: '***REMOVED***',
-        external: 'bs3yDrxrcWUfzWOsMCe2EbLMXl4nWs7x93mkIaNkCvs'
-    },
-    robot:'https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=c3f73dde-bac6-46ad-8047-06dde1c001c8',
-}
