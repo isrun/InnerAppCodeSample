1903154737f5cd0f10da806051c7358f97733573
caiozhang <caiozhang@tencent.com>
2021-08-27 11:51:43 +0800

    feat: 缓存token

--- main.config.js
+++ main.config.js
@@ -1,8 +1,8 @@
 module.exports = {
     corpid:'ww827c549fa062654e',
-    secret:'***REMOVED***',
+    secret:{
+        common: '***REMOVED***',
+        contact: '***REMOVED***'
+    },
     robot:'https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=c3f73dde-bac6-46ad-8047-06dde1c001c8',
-    address: {
-        secret: '***REMOVED***'
-    }
 }
