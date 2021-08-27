fc70924c9d7735fb1d64e5f72d5776286492a55c
caiozhang <caiozhang@tencent.com>
2021-08-27 16:27:11 +0800

    chore: 新增根目录创建成员与部门

--- main.config.js
+++ main.config.js
@@ -2,7 +2,8 @@ module.exports = {
     corpid:'ww827c549fa062654e',
     secret:{
         common: '***REMOVED***',
-        contact: '***REMOVED***'
+        contact: '***REMOVED***',
+        external: 'bs3yDrxrcWUfzWOsMCe2EbLMXl4nWs7x93mkIaNkCvs'
     },
     robot:'https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=c3f73dde-bac6-46ad-8047-06dde1c001c8',
 }
